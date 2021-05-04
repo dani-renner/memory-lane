302472a1c935b5513ec168a096127c8dc0c9b50c
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-04 12:38:49 -0500

    add form visible reducer
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,10 @@
+import { combineReducers } from 'redux';
+import formVisibleReducer from "./form-visible-reducer";
 import { firestoreReducer } from 'redux-firestore';
 
 const rootReducer = combineReducers({
+  formVisibleOnPage: formVisibleReducer,
   firestore: firestoreReducer
 });
+
+export default rootReducer;
