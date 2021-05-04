ae777ea94d72822912807b32b14afbac5460b672
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-04 12:32:30 -0500

    add reusable form for creating/editing memories
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type="text"
+          name="title"
+          placeholder="title" />
+        <input
+          type="date"
+          name="date"
+          placeholder="date" />
+        <textarea
+          name="memory"
+          placeholder="memory" />
+        <button type="submit">{props.buttonText}</button>
+      </form>
+    </>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
