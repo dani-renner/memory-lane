7dfb9306566a9aeadadd814d5a16aade8b0dd9c8
Morgan Bradford <morganjbradford95@gmail.com>
2021-05-04 17:36:29 -0500

    add fetch
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- src/components/SearchPhotos.js
+++ src/components/SearchPhotos.js
@@ -1,14 +1,21 @@
 import React, { useState } from "react";
-import Unsplash, { toJson } from "unsplash-js";
+import fetch from 'node-fetch';
+import Unsplash, { toJson } from 'unsplash-js';
 
-export default function SearchPhotos() {
+
+const unsplash = new Unsplash({
+  accessKey: `${process.env.REACT_APP_UNSPLASH_API_KEY}`,
+  fetch: nodeFetch,
+});
+
+function SearchPhotos() {
   const [query, setQuery] = useState("");
   const [pics, setPics] = useState([]);
 
   const searchPhotos = async (e) => {
     e.preventDefault();
     unsplash.search
-      .photos(query)
+      .photos(query, 1, 20)
       .then(toJson)
       .then((json) => {
         setPics(json.results);
@@ -16,7 +23,25 @@ export default function SearchPhotos() {
   };
   return (
     <>
+    <form className="form">
+        <label className="label" htmlFor="query">
+          {" "}
+        </label>
+        <input
+          type="text"
+          name="query"
+          className="input"
+          placeholder={`Try "dog" or "apple"`}
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit" className="button">
+          Search
+        </button>
+      </form>
       <form className="form" onSubmit={searchPhotos}></form>
     </>
   );
 }
+
+export default SearchPhotos;
