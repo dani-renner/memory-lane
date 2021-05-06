69fa5bc623e16f27429e11bf74c0b76e490e7fd6
Dani Renner <danijrenner@gmail.com>
2021-05-06 11:36:31 -0700

    try using async fetch instead for API
    Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>

--- src/components/SearchPhotos.js
+++ src/components/SearchPhotos.js
@@ -4,15 +4,44 @@ import React, { useState, useEffect } from "react";
 import fetch from 'node-fetch';
 global.fetch = fetch;
 
-const unsplash = new Unsplash({
-  accessKey: process.env.REACT_APP_UNSPLASH_API_KEY
-  // fetch: nodeFetch,
-});
+// const unsplash = new Unsplash({
+//   accessKey: process.env.REACT_APP_UNSPLASH_API_KEY
+//   // fetch: nodeFetch,
+// });
 
 function SearchPhotos() {
   const [query, setQuery] = useState("");
   const [pics, setPics] = useState([]);
 
+
+  const searchPhotos = async (e) => {
+    e.preventDefault();
+    fetch(`https://api.unsplash.com/search/photos/?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}&query=${query}&page=1&per_page=1&order_by=latest`)
+    .then((response) => {
+    setPics(response) 
+    })
+    .catch(console.log)
+  }
+// for (let i = 0; i < 20; i++) {
+//   //  array.push(fetchResponse.results[i].urls.full);
+
+// }
+//  fetchResponse.results[i].urls.full
+
+
+
+
+
+
+  // const apiURL = `https://api.unsplash.com/search/photos/?client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}&query=${query}}`;
+    // const searchPhotos = async () => {
+    //     fetch(apiURL)
+    //     // .then(response => response.json())
+    //     .then((result) => {
+    //       setPics(result.json) 
+    //     })
+    //     .catch(console.log)
+    //     }
 
   // useEffect(() => {
   //   unsplash.search
@@ -25,18 +54,18 @@ function SearchPhotos() {
   //     });
   // }, []);
 
-  const searchPhotos = async (e) => {
-    e.preventDefault();
-    unsplash.search
-      .getPhotos({
-        query: query,
-        page: 1,
-        perPage: 20
-      })
-      .then(result => {
-        setPics(result);
-      })
-  };
+  // const searchPhotos = async (e) => {
+  //   e.preventDefault();
+  //   unsplash.search
+  //     .getPhotos({
+  //       query: query,
+  //       page: 1,
+  //       perPage: 20
+  //     })
+  //     .then(result => {
+  //       setPics(result);
+  //     })
+  // };
   return (
     <>
     <form className="form">
